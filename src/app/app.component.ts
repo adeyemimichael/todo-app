0ecb98f215d23c3d245020df52e97e3f01da4c68
adeyemimike123 <ayobami732000@gmail.com>
2023-03-17 14:01:33 +0100

    update the build production command

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Todo-app';
+  //declaration of an empty array
+  list:any[] =[]
+  //this helps in adding the to do item to the collection
+  addTask(item:string){
+console.warn(item)
+
+this.list.push({id:this.list.length,name:item})
+console.warn(this.list)
+  }
+  //this removes  the list items from the collection of items
+  removeTask(id:number){
+console.warn(id)
+this.list =this.list.filter(item => item.id!== id);
+  }
 }
